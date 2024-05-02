cfa61770852a770f20f03918c7e6d47d981057ac
KENNETH <KENNETH@DESKTOP-FTK6SDF>
2024-05-02 21:30:13 +0800

    hello

--- Payroll/src/main/java/com/mycompany/payroll/Payroll.java
+++ Payroll/src/main/java/com/mycompany/payroll/Payroll.java
@@ -16,5 +16,6 @@ public class Payroll {
         System.out.print("2nd change");
         System.out.print("3rd change");
         System.out.print("nice one");
+        System.out.print("nandito ako umiibig sayo :>");
     }
 }
