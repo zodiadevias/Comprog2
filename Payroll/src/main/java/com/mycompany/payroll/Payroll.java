758760caddfdb07bde9d40240c0349a76e29b309
Byron <dizon.lordbyron@gmail.com>
2024-05-02 20:57:07 +0800

    added payroll.java

---
+++ Payroll/src/main/java/com/mycompany/payroll/Payroll.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.payroll;
+
+/**
+ *
+ * @author ZodiaDevias
+ */
+public class Payroll {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
